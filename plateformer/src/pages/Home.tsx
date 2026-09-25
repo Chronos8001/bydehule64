aa8920d7c1e34aa481ac46f0dac9468e3575b639
Simon <simon.pereiraparada@ynov.com>
2026-09-25 18:50:33 +0200

    menu song like wow lol

--- plateformer/src/pages/Home.tsx
+++ plateformer/src/pages/Home.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button } from '../components/ui/Button';
+import { useMenuAudio } from '../hooks/useMenuAudio';
 import './Home.css';
 
 export const Home: React.FC = () => {
   const navigate = useNavigate();
+  const { menuMusic, musicRef, startMenuMusic } = useMenuAudio();
+
   return (
-    <div className="home-menu">
+    <div className="home-menu" onPointerDown={startMenuMusic}>
+      <audio ref={musicRef} src={menuMusic} loop preload="auto" />
       <h1 className="home-title">bidule 64</h1>
       <div className="home-screen-heading">Select a Game</div>
       <div className="home-menu-buttons">
